c0b98bc359ba7442cc31d25359e5d6e80ebe7456
lazare.giorgobiani <lazare.giorgobiani@lb.ge>
2022-12-22 14:17:25 +0400

    Added characterReplacement(Prob 424) full solution

--- LeetCodeProbProj/src/CharacterReplacement424.java
+++ LeetCodeProbProj/src/CharacterReplacement424.java
@@ -5,54 +5,21 @@ import java.util.Set;
 
 public class CharacterReplacement424 {
     public static int characterReplacement(String s, int k){
-        HashMap<Character, ArrayList<Integer>> mp = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(!mp.containsKey(c)) mp.put(c, new ArrayList<>());
-            mp.get(c).add(i);
-        }
-        if(mp.size() == 1) return s.length();
-        int maxLength = Integer.MIN_VALUE;
-        for(char c : mp.keySet()){
-            ArrayList<Integer> indexes = mp.get(c);
-            System.out.println(indexes);
-            int startIndex = indexes.get(0) >= k ? indexes.get(0) - k : 0;
-            int lastIndex = indexes.get(indexes.size()-1);
-            lastIndex = (lastIndex < s.length() - k) ? lastIndex + k : s.length()-1;
-            System.out.println("startIndex - " + startIndex + " lastIndex - " + lastIndex);
-            int l = startIndex;
-            int replaced = 0;
-            Set<Integer> st = new HashSet<>();
-            boolean changed = false;
-            for(int i=startIndex; i<=lastIndex; i++){
-                if(!indexes.contains(i)){
-                    st.add(i);
-                    replaced++;
+        Set<Character> uniqueChars = new HashSet<Character>();
+        for(int i=0; i<s.length(); i++) uniqueChars.add(s.charAt(i));
+        int maxWindowSize = 0;
+        for(char c : uniqueChars){
+            int start = 0;
+            int charCount = 0;
+            for(int end = 0; end < s.length(); end++){
+                if(s.charAt(end) == c) charCount++;
+                while(end + 1 - start - charCount > k){
+                    if(s.charAt(start) == c) charCount--;
+                    start++;
                 }
-                if(replaced == k){
-                    System.out.println("i - " + i);
-                    changed = true;
-                    int p = l;
-                    int counter = 0;
-                    //char firstChar = s.charAt(p);
-                    System.out.println("firstChar - " + c);
-                    System.out.println("set - " + st);
-                    while(p < s.length() && (st.contains(p) || c == s.charAt(p))){
-                        System.out.print(p);
-                        p++;
-                        counter++;
-                    }
-                    System.out.println();
-                    System.out.println("start - " + l + " end - " + (l+counter));
-                    if(counter > maxLength) maxLength = counter;
-                    while(indexes.contains(l)) l++;
-                    st.remove(l);
-                    replaced--;
-                    l++;
-                }
+                maxWindowSize = Integer.max(end+1-start, maxWindowSize);
             }
-            if(!changed) return s.length();
         }
-        return maxLength;
+        return maxWindowSize;
     }
 }
--- LeetCodeProbProj/src/Main.java
+++ LeetCodeProbProj/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "";
-        int res = CharacterReplacement424.characterReplacement(str, 100);
+        String str = "ABBAB";
+        int res = CharacterReplacement424.characterReplacement(str, 1);
         System.out.println(res);
     }
 }
